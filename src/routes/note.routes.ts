4af510bbe9f50b0ea81f3d1b0acbbd7bdc23d308
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-08-25 14:16:54 +0300

    add /stats and /tags endpoints to note.routes.ts with auth middleware

--- src/routes/note.routes.ts
+++ src/routes/note.routes.ts
@@ -6,6 +6,8 @@ const router = Router();
 
 router.post('/', auth, noteController.create);
 router.get('/', auth, noteController.getAll);
+router.get('/stats', auth, noteController.getStats);
+router.get('/tags', auth, noteController.getTags);
 router.get('/:id', auth, noteController.getById);
 router.put('/:id', auth, noteController.update);
 router.delete('/:id', auth, noteController.remove);
